dd9c7386f16ea4229302f73d5d4b9a126cbbedab
Leo B. <me@leobreuer.dev>
2024-11-18 21:26:00 +0100

    feat: add gradle bums

--- src/main/kotlin/one/orbiq/sproutbound/Sproutbound.kt
+++ src/main/kotlin/one/orbiq/sproutbound/Sproutbound.kt
@@ -1,11 +1,6 @@
 package one.orbiq.sproutbound
 
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents.ServerStarting
-import net.minecraft.server.MinecraftServer
-import net.minecraft.util.Identifier
-
 
 class Sproutbound : ModInitializer {
     companion object {
